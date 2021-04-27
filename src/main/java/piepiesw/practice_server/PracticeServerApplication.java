da93ababc2afa618a3241ce6817615de8f2137bf
seungwon2 <dandelionsw2@ewhain.net>
2021-04-27 18:35:53 +0900

    :building_construction: build : build project by spring boot

---
+++ src/main/java/piepiesw/practice_server/PracticeServerApplication.java
@@ -0,0 +1,13 @@
+package piepiesw.practice_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/piepiesw/practice_server/PracticeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package piepiesw.practice_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
